1b7c7b70960a4651d0ad869cfb50466da4645809
paulnagle <paulnagle@gmail.com>
2018-01-15 15:13:52 +0000

    Clean up home page

--- src/pages/home/home-component/home.component.ts
+++ src/pages/home/home-component/home.component.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, Events, MenuController } from 'ionic-angular';
 
 import { AboutComponent } from '../../about/about-component/about.component';
-import { WordpressHome } from '../../wordpress/wordpress-home/wordpress-home.component';
-import { WordpressTags } from '../../wordpress/wordpress-tags/wordpress-tags.component';
-import { WordpressFavorites } from '../../wordpress/wordpress-favorites/wordpress-favorites.component';
-import { WordpressPages } from '../../wordpress/wordpress-pages/wordpress-pages.component';
-import { WordpressPage } from '../../wordpress/wordpress-page/wordpress-page.component';
-import { FeedCategoriesComponent } from '../../feeds/feed-categories/feed-categories.component';
-import { FeedCategoryComponent } from '../../feeds/feed-category/feed-category.component';
 
 @Component({
 	selector: 'page-home',
@@ -23,13 +16,7 @@ export class HomeComponent {
 
 	ngOnInit() {
 	  	this.pages = [
-	      { title: 'ABOUT', component: AboutComponent, icon: 'photos', note: '' },
-	      { title: 'TAGS', component: WordpressTags, icon: 'pricetags', note: 'Wordpress' },
-	      { title: 'FAVORITES', component: WordpressFavorites, icon: 'heart', note: 'Wordpress (Storage)' },
-	      { title: 'PAGES', component: WordpressPages, icon: 'document', note: 'Wordpress' },
-				{ title: 'PAGE', component: WordpressPage, icon: 'document', note: 'Wordpress', params: { id: 2 }},
-	      { title: 'FEEDS', component: FeedCategoriesComponent, icon: 'logo-rss', note: 'RSS (YQL)' },
-	      { title: 'FEED_CATEGORY', component: FeedCategoryComponent, icon: 'logo-rss', note: 'RSS (YQL)' }
+	      { title: 'ABOUT', component: AboutComponent, icon: 'photos', note: '' }
 	    ];
 
 	    this.events.subscribe('navigationEvent',(object) => {
