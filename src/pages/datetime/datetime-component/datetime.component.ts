b0a8115a5f7b719dbfe273dff1e2f3a193b4b0b2
paulnagle <paulnagle@gmail.com>
2018-02-11 12:04:40 +0000

    Cleantime first pass

--- src/pages/datetime/datetime-component/datetime.component.ts
+++ src/pages/datetime/datetime-component/datetime.component.ts
@@ -4,12 +4,52 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: 'datetime.html'
 })
+
 export class DatetimeComponent {
-  public event = {
-    month: '1990-02-19',
-    timeStarts: '07:43',
-    timeEnds: '1990-02-20'
+
+  myDate: any;
+  cleanTimeInMilliseconds : any;
+  todayInMilliseconds : any;
+  cleanTimeInDays : any;
+  cleanTimeInWeeks : any;
+  cleanTimeInYears : any;
+
+  constructor() {
+    this.myDate = new Date().toISOString();
+    this.cleanTimeInMilliseconds = Date.parse(this.myDate);
+    this.todayInMilliseconds = Date.parse(this.myDate);
   }
 
+  getCleanTime(){
+        var day, hour, minute, seconds;
+
+        this.cleanTimeInMilliseconds = Date.parse(this.myDate);
+        this.cleanTimeInMilliseconds = this.todayInMilliseconds - this.cleanTimeInMilliseconds;
+
+        seconds = Math.floor(this.cleanTimeInMilliseconds / 1000);
+        minute = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        hour = Math.floor(minute / 60);
+        minute = minute % 60;
+        this.cleanTimeInDays = Math.floor(hour / 24);
+        hour = hour % 24;
+        return this.cleanTimeInDays
+
+  }
+
+  getCleanTimeWeeks() {
+    this.cleanTimeInWeeks = Math.floor(this.cleanTimeInDays / 7);
+
+    return this.cleanTimeInWeeks;
+  }
+
+  getCleanTimeYears() {
+    this.cleanTimeInMilliseconds = Date.parse(this.myDate);
+    this.cleanTimeInMilliseconds = this.todayInMilliseconds - this.cleanTimeInMilliseconds;
+
+    this.cleanTimeInYears = Math.floor(this.cleanTimeInMilliseconds / 31536000000);
+
+    return this.cleanTimeInYears;
+  }
 
 }
