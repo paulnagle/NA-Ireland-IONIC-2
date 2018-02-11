4962a57eee3581a60c0d3faf1f7186aa92e09667
paulnagle <paulnagle@gmail.com>
2018-02-11 22:20:37 +0000

    Android: Open google maps app if installed for maps link

--- src/pages/google-maps/google-maps-component/google-maps.component.ts
+++ src/pages/google-maps/google-maps-component/google-maps.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LoadingController } from 'ionic-angular';
-import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
+import { Platform } from 'ionic-angular';
 import { MeetingListProvider } from '../../../providers/meeting-list/meeting-list';
 
 @Component({
@@ -8,30 +8,12 @@ import { MeetingListProvider } from '../../../providers/meeting-list/meeting-lis
 })
 export class GoogleMapsComponent {
 
-  options : InAppBrowserOptions = {
-      location : 'yes',//Or 'no'
-      hidden : 'no', //Or  'yes'
-      clearcache : 'yes',
-      clearsessioncache : 'yes',
-      zoom : 'yes',//Android only ,shows browser zoom controls
-      hardwareback : 'yes',
-      mediaPlaybackRequiresUserAction : 'no',
-      shouldPauseOnSuspend : 'no', //Android only
-      closebuttoncaption : 'Close', //iOS only
-      disallowoverscroll : 'no', //iOS only
-      toolbar : 'yes', //iOS only
-      enableViewportScale : 'no', //iOS only
-      allowInlineMediaPlayback : 'no',//iOS only
-      presentationstyle : 'pagesheet',//iOS only
-      fullscreen : 'yes',//Windows only
-  };
-
   meetingList : any;
 
   loader = null;
   constructor(private MeetingListProvider : MeetingListProvider,
               public loadingCtrl: LoadingController,
-              private theInAppBrowser: InAppBrowser) {
+              public plt: Platform) {
 
     this.loader = this.loadingCtrl.create({
           content: "Loading Meeting Map..."
@@ -64,10 +46,17 @@ export class GoogleMapsComponent {
     });
   }
 
-  public openMapsLink(destLatitude, destLongitude){
-    let target = "_blank";
-    let url = "https://www.google.com/maps/search/?api=1&query=" + destLatitude + ',' + destLongitude;
-    this.theInAppBrowser.create(url,target,this.options);
+  public openMapsLink(destLatitude, destLongitude) {
+    // ios
+    if (this.plt.is('ios')) {
+      window.open('maps://&daddr=' + destLatitude + ',' + destLongitude, '_system');
+    };
+    // android
+    if (this.plt.is('android')) {
+      window.open('https://www.google.com/maps/search/?api=1&query=' + destLatitude + ',' + destLongitude + ')', '_system');
+    };
+
+
   }
 
 }
