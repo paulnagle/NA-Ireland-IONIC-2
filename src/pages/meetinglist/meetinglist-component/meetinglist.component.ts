3639e51a21e1d493f7c332abe54fc555a4488e15
paulnagle <paulnagle@gmail.com>
2018-02-18 18:17:48 +0000

    Revert "Timeout for meetings list"
    This reverts commit 155fd8905e916b07fbb031efc58d73a4ea158bf5.

--- src/pages/meetinglist/meetinglist-component/meetinglist.component.ts
+++ src/pages/meetinglist/meetinglist-component/meetinglist.component.ts
@@ -3,7 +3,6 @@ import { Platform } from 'ionic-angular';
 import { MeetingListProvider } from '../../../providers/meeting-list/meeting-list';
 
 import { LoadingController } from 'ionic-angular';
-import 'rxjs/add/operator/timeout';
 
 @Component({
   selector: 'page-meetinglist',
@@ -85,8 +84,7 @@ export class MeetinglistComponent {
               public plt: Platform) {
 
     this.loader = this.loadingCtrl.create({
-          content: "Loading Meeting List...",
-          duration: 15000
+          content: "Loading Meeting List..."
         });
     this.loader.present();
 
@@ -202,10 +200,7 @@ export class MeetinglistComponent {
   isWicklowShown(group) { return this.shownWicklow === group; };
 
   getAllMeetings(){
-    this.MeetingListProvider
-        .getMeetings()
-        .timeout(15000)
-        .subscribe((data)=>{
+    this.MeetingListProvider.getMeetings().subscribe((data)=>{
       this.meetingList   = data;
       this.AntrimList    = this.meetingList.filter(meeting => meeting.location_sub_province == "Antrim");
       this.ArmaghList    = this.meetingList.filter(meeting => meeting.location_sub_province == "Armagh");
@@ -241,13 +236,6 @@ export class MeetinglistComponent {
       this.WicklowList   = this.meetingList.filter(meeting => meeting.location_sub_province == "Wicklow");
 
       this.loader.dismiss();
-    }, (errorResponse: any) =>{
-      this.loader.dismiss();
-      this.loader = this.loadingCtrl.create({
-              content: "Request timed out",
-              duration: 5000
-            });
-      this.loader.present();
     });
   }
 }
