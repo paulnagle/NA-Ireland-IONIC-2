5fcc37fe83693c1084411adf35f54d561b1ed9ad
paulnagle <paulnagle@gmail.com>
2018-04-16 20:53:16 +0100

    thenby npm install

--- re-plugin.sh
+++ re-plugin.sh
@@ -11,13 +11,13 @@ ionic cordova plugin rm cordova-plugin-splashscreen
 ionic cordova plugin rm cordova-plugin-statusbar
 ionic cordova plugin rm cordova-plugin-whitelist
 
+ionic cordova platform add ios
+ionic cordova platform add android
+
 ionic cordova plugin add cordova-plugin-inappbrowser
 ionic cordova plugin add cordova-plugin-insomnia
 ionic cordova plugin add cordova-plugin-splashscreen
 ionic cordova plugin add cordova-plugin-statusbar
 ionic cordova plugin add cordova-plugin-whitelist
-
-ionic cordova platform add ios
-ionic cordova platform add android
 
 ionic cordova prepare
